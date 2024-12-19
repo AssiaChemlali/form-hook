9baa1b86ea493a5dbbc183f68d842d40074042c3
AssiaChemlali <chemlali.assia@gmail.com>
2024-12-19 13:44:21 +0100

    add useForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import { useState } from 'react'
 import './App.css'
+import { useForm } from 'react-hook-form'
 
 function App() {
-
+const {register,handleSubmit}=useForm()
+ 
+const onSubmit= data=>{
+  console.log('data',data)
+}
   return (
-    <form className='flex flex-col p-10 gap-2 bg-gray-100'>
+    <form 
+    onSubmit={handleSubmit(onSubmit)}
+    className='flex flex-col p-10 gap-2 bg-gray-100'>
      <label >UserName</label>
-     <input type="text"  className='border p-2'/>
+     <input {...register('username')}  type="text"  className='border p-2'/>
 
      <label>Email</label>
-     <input type="email" className='border p-2'/>
+     <input  {...register('email')}type="email" className='border p-2'/>
 
      <label>Password</label>
-     <input type="password" className='border p-2' />
+     <input {...register('password')} type="password" className='border p-2' />
 
      <button type='submit' className='bg-blue-500 text-white p-2 font-bold mt-4'>Register</button>
 
