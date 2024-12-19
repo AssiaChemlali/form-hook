13a3de83cbc5d007c9af2e969721318894b6d3e6
AssiaChemlali <chemlali.assia@gmail.com>
2024-12-19 13:35:35 +0100

    add form

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,19 @@ import './App.css'
 function App() {
 
   return (
-    <>
-  
-    </>
+    <form className='flex flex-col p-10 gap-2 bg-gray-100'>
+     <label >UserName</label>
+     <input type="text"  className='border p-2'/>
+
+     <label>Email</label>
+     <input type="email" className='border p-2'/>
+
+     <label>Password</label>
+     <input type="password" className='border p-2' />
+
+     <button type='submit' className='bg-blue-500 text-white p-2 font-bold mt-4'>Register</button>
+
+    </form>
   )
 }
 
